7e24ce5d80dfb74f9c8584a448965b0ea721a6f4
Oliver-Gomes <ogomesphoto@gmail.com>
2018-08-15 17:17:14 -0400

    setTimeout to remove the open class if two card don't match

--- js/app.js
+++ js/app.js
@@ -55,7 +55,7 @@ function shuffle(array) {
  * DONE - display the card's symbol (put this functionality in another function that you call from this one)
  * DONE - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ * DONE + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
@@ -82,8 +82,14 @@ function flipCard(){
         if (firstCard.childNodes[0].className === secondCard.childNodes[0].className){
             firstCard.removeEventListener('click', flipCard);
             secondCard.removeEventListener('click', flipCard);
+        } else {
+            setTimeout(function(){
+                firstCard.classList.remove('open')
+                firstCard.classList.remove('show')
+                secondCard.classList.remove('open')
+                secondCard.classList.remove('show')
+            }, 1500);   
         }
-        console.log('Function was executed!')
     }
 };
 
